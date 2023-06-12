ac35b4cb6f2030cca8a8dc1e3567b5200c971328
JulieBodette <jr3python@gmail.com>
2023-06-12 13:50:00 -0400

    Initial commit

---
+++ WisejMVC/Page1.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System;
+using Wisej.Web;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace WisejMVC
+{
+	public partial class Page1 : Page
+	{
+		private StudentController controller;
+		public Page1()
+		{
+			InitializeComponent();
+			controller = new StudentController();
+		}
+		//DOES NOT WORK
+		//List<StudentModel> PeopleList = new List<StudentModel>();
+		BindingList<StudentModel> PeopleList = new BindingList<StudentModel>();
+
+		private void Page1_Load(object sender, System.EventArgs e)
+		{
+			//DOES NOT WORK
+			//PeopleList = controller.GetStudents();
+			//dataGridView1.DataSource = PeopleList;
+			dataGridView1.DataSource = PeopleList;
+			StudentModel model = new StudentModel() { Name = "John", Id = 12345, Age = 30, Email = "John@school.com" };
+			model.Id = 10;
+			PeopleList.Add(model);
+			PeopleList.Add(new StudentModel() { Name = "Julie", Id = 11111, Age = 26, Email = "Julie@example.com" });
+			PeopleList.Add(new StudentModel() { Name = "Jane", Id = 76394, Age = 24, Email = "Jane-email@domain.com" });
+			PeopleList.Add(new StudentModel() { Name = "Jack", Id = 9, Age = 19, Email = "Jack_is_greatgmail.com" });
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+			//example of valid data
+			//StudentModel model = new StudentModel() { Name = "John", Id =1, Age = 30, Email = "John@school.com" };
+
+			//example of invalid data
+			//StudentModel model = new StudentModel() { Name = "John", Id = 999999, Age = 30, Email = "bademail" };
+
+
+			//read the data from the view
+			int id = Int32.Parse(txtId.Text);
+			string email = txtEmail.Text;
+			string name = txtName.Text;
+			int age = Int32.Parse(txtAge.Text);
+
+			//controller does this- need to move this code to controller
+			string errorMessage = controller.AddStudent(name, id, age, email);
+			AlertBox.Show(errorMessage);
+			//also need to save data to database (do this in the AddStudent method)
+
+			//clear the textboxes
+			txtId.Text = "";
+			txtEmail.Text = "";
+			txtName.Text = "";
+			txtAge.Text = "";
+			
+		}
+
+	}
+}
---
+++ WisejMVC/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Configuration;
+using System.Security.Cryptography;
+using Wisej.Web;
+
+namespace WisejMVC
+{
+	public class StudentController
+	{
+
+		public string AddStudent(string name, int id, int age, string email)
+		{
+			//attempts to add a student to the database
+			//returns a string with the validation errors
+			StudentModel model = new StudentModel() { Name = name, Id = id, Age = age, Email = email };
+			string errorMessage = StudentModel.ValidateData(model);
+
+			//add code here that calls a method to add to the database
+			//StudentModel needs to have the method that interacts with the database itself.
+			return errorMessage;
+		}
+
+		//get the database connection string
+		//THIS CODE DOES NOT WORK
+		//public static string CnnVal(string name)
+		//{
+		//	return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+		//}
+
+		//public List<StudentModel> GetStudents()
+		//{
+		//	using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(StudentController.CnnVal("Students")))
+		//	{
+		//		//using SQL query sent in by text
+		//		var output = connection.Query<StudentModel>("select * from Students").ToList();
+		//		return output;
+		//	}
+		//}
+	}
+}
